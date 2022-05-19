81410b5d489e248e2954a9653f6d9daa81e36d9b
Maksim Ivlev <brainlucker@gmail.com>
2022-05-19 23:51:47 +0300

    DB Interaction added

---
+++ src/test/java/ru/netology/shop/db/DbInteraction.java
@@ -0,0 +1,76 @@
+package ru.netology.shop.db;
+
+import io.qameta.allure.Step;
+import lombok.SneakyThrows;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbInteraction {
+    private final String url = "jdbc:postgresql://localhost:5432/app";
+    private final String user = "admin";
+    private final String password = "pass";
+    private final String paymentTable = "payment_entity";
+    private final String creditTable = "credit_request_entity";
+    private final String interval = "now() - interval '30 seconds'";
+
+    @SneakyThrows
+    private Connection getConnection() {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    @Step("Получаем статус покупки из {0}")
+    @SneakyThrows
+    private String getStatus(String table) {
+        var statusSQL = "SELECT status FROM " + table +
+                " WHERE created > " + interval +
+                " ORDER BY created DESC";
+
+        try (var conn = getConnection()) {
+            return new QueryRunner().query(conn, statusSQL, new ScalarHandler<>());
+        }
+    }
+
+    public String getPaymentStatus() {
+        return getStatus(paymentTable);
+    }
+
+    public String getCreditStatus() {
+        return getStatus(creditTable);
+    }
+
+    @Step("Получаем сумму оплаты из {paymentTable}")
+    @SneakyThrows
+    public int getPaymentAmount() {
+        var amountSQL = "SELECT amount/100 FROM " + paymentTable +
+                " WHERE created > " + interval +
+                " ORDER BY created DESC";
+
+        try (var conn = getConnection()) {
+            return new QueryRunner().query(conn, amountSQL, new ScalarHandler<>());
+        }
+    }
+
+    @Step("Получаем id заказа в таблице «order_entity» для покупки из «{0}»")
+    @SneakyThrows
+    private String getOrderId(String table, String joinOnColumns) {
+        var orderIdSQL = "SELECT oe.id FROM " + table +
+                " INNER JOIN order_entity oe ON " + joinOnColumns +
+                " WHERE oe.created > " + interval +
+                " ORDER BY oe.created DESC";
+
+        try (var conn = getConnection()) {
+            return new QueryRunner().query(conn, orderIdSQL, new ScalarHandler<>());
+        }
+    }
+
+    public String getPaymentOrderId() {
+        return getOrderId(paymentTable, "transaction_id = payment_id");
+    }
+
+    public String getCreditOrderId() {
+        return getOrderId(creditTable, "bank_id = credit_id");
+    }
+}
