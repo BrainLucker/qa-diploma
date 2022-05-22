b0d6460a63f8df17e843ef63eb7c0fd7ae7d3b51
Maksim Ivlev <brainlucker@gmail.com>
2022-05-23 00:01:01 +0300

    Order class added

---
+++ src/test/java/ru/netology/shop/mode/Order.java
@@ -0,0 +1,35 @@
+package ru.netology.shop.mode;
+
+import io.qameta.allure.Step;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@AllArgsConstructor
+@Value
+public class Order {
+    String id;
+    String status;
+    int amount;
+
+    public Order(String status, int amount) {
+        this.id = "1";
+        this.status = status;
+        this.amount = amount;
+    }
+
+    @Step("Проверяем, что заказ внесен в БД корректно, его статус «{expectedOrder.status}», а сумма {expectedOrder.amount}.")
+    public void assertPaymentOrder(Order expectedOrder) {
+        assertEquals(expectedOrder.getStatus(), this.status);
+        assertEquals(expectedOrder.getAmount(), this.amount);
+        assertNotNull(this.getId());
+    }
+
+    @Step("Проверяем, что заказ внесен в БД корректно, а его статус «{expectedOrder.status}».")
+    public void assertCreditOrder(Order expectedOrder) {
+        assertEquals(expectedOrder.getStatus(), this.status);
+//        assertNotNull(this.getId());
+    }
+}
